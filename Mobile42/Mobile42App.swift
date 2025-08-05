283058fd79f171cff7280e0724922a20f27886c8
quietmid <ninjonglu@gmail.com>
2025-08-05 14:01:01 +0300

    Initial Commit

---
+++ Mobile42/Mobile42App.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile42App.swift
+//  Mobile42
+//
+//  Created by Joseph Lu on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Mobile42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
